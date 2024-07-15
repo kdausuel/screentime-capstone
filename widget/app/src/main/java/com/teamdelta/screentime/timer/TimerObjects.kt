5c246d6068e12527833d6ee7b2719bd8fc3b174b
kdausuel <kalim.dausuel@gmail.com>
2024-07-14 23:16:15 -0500

    Refactored timer folder; created Timer and TimerObjects

---
+++ widget/app/src/main/java/com/teamdelta/screentime/timer/TimerObjects.kt
@@ -0,0 +1,19 @@
+package com.teamdelta.screentime.timer
+
+/**
+ * Object representing the session timer.
+ *
+ * This object manages the state and operations related to the session timer.
+ */
+object SessionTimer : Timer("session") {
+
+}
+
+/**
+ * Object representing the daily timer.
+ *
+ * This object manages the state and operations related to the daily timer.
+ */
+object DailyTimer : Timer("daily"){
+
+}
